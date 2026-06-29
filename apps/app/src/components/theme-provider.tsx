199ccd30da669478692a10b26786565df2e9dca3
Conar Welsh <conarw@gmail.com>
2026-06-29 10:23:57 -0400

    fix: disable nestjs console clearing and implement sw killer

--- apps/app/src/components/theme-provider.tsx
+++ apps/app/src/components/theme-provider.tsx
@@ -4,5 +4,15 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === "development" && "serviceWorker" in navigator) {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (const registration of registrations) {
+          registration.unregister();
+        }
+      });
+    }
+  }, []);
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
