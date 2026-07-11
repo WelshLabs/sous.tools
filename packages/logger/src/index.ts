f6220541d55c45180ef6bec36ebf2431e5341540
Conar Welsh <conarw@gmail.com>
2026-07-11 13:58:55 -0400

    chore: AI implementation for packages/logger/src/index.ts [skip ci]

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,1 +1,12 @@
-export * from './server.js';
+import initializeBrowserLogger from './browser';
+import initializeServerLogger from './server';
+
+const initializeLogger = () => {
+    if (typeof window === 'undefined') {
+        initializeServerLogger();
+    } else {
+        initializeBrowserLogger();
+    }
+};
+
+export { initializeLogger, initializeBrowserLogger, initializeServerLogger };
