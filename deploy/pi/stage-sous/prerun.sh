58038be467250034e65d4a597cb6a2dc08b4ae43
Conar Welsh <conarw@gmail.com>
2026-07-22 17:11:29 -0400

    fix rpi os build

--- deploy/pi/stage-sous/prerun.sh
+++ deploy/pi/stage-sous/prerun.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 
+# Install wl-mirror dependencies
+apt-get install -y cmake meson wayland-protocols libwayland-dev libgles2-mesa-dev
+# Clone and build wl-mirror
+git clone https://github.com/Ferdi265/wl-mirror.git /tmp/wl-mirror
+cd /tmp/wl-mirror
+mkdir build && cd build
+cmake ..
+make && make install
+
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
